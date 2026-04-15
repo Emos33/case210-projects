c86bb0ca57e2f1ba43960c89e73ac20f0c4a2618
Emos33 <abello1@byupathway.edu>
2026-04-15 11:29:46 +0100

    Activity Tracking wk7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+// Base Class
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    // Abstract methods (Polymorphism)
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    // Shared method
+    public virtual string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min) - " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace: {GetPace():0.00} min per km";
+    }
+}
